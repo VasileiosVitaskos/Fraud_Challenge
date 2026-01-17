5cddb5e3044085ded29460597a1b19be4b10afc7
VasileiosVitaskos <vvitaskos@gmail.com>
2026-01-17 13:12:37 +0200

    Finallized project cleanup

--- test_setup.py
+++
@@ -1,9 +0,0 @@
-from src.common.redis_client import get_redis_client
-try:
-   
-    r= get_redis_client()
-    r.set("msg", "Hello from Vim")
-    print("YES DONE: " + r.get("msg"))
-except Exception as e:
-    print(e)
-
